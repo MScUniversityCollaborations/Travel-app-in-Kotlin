5290ed16f6ef18e3dbe34ea4e21edb0bba5e2344
Νικόλας Π <nikolas.98@live.com>
2021-12-18 04:43:34 +0200

    Added dependencies that we will need.

---
+++ app/src/main/java/com/unipi/torpiles/cyprustraveler/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.unipi.torpiles.cyprustraveler.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.unipi.torpiles.cyprustraveler.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
